ef1057dbdb7201837bc46ab926aff481265593d8
Ariestellar <ahdrei47@gmail.com>
2020-09-04 19:40:43 +0300

    Init

--- Assets/Scripts/Patient/PatientData.cs
+++ Assets/Scripts/Patient/PatientData.cs
@@ -1,15 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 [CreateAssetMenu(fileName = "NewPatientData", menuName = "PatientData", order = 51)]
 public class PatientData : ScriptableObject
 {
-    [SerializeField] private Sprite _appearance;
-    [SerializeField] private Animator _animation;    
-    [SerializeField] private GameObject _animationGO;    
+    [SerializeField] private String _name;   
+    [SerializeField] private GameObject _animation;    
 
-    public Sprite Appearance => _appearance;
-    public Animator Animation => _animation;
-    public GameObject AnimationGO => _animationGO;
+    public GameObject AnimationGO => _animation;
 }
